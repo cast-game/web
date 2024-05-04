99cc54472b48e4aad9eb4e2fe722d723922f50bd
Ben Baessler <mail@benbaessler.com>
2024-05-04 21:44:54 +0100

    feat: round overview card ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
 import Navbar from "./components/Navbar";
+import Leaderboard from "./components/Leaderboard";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
+      <Leaderboard />
     </main>
   );
 }
