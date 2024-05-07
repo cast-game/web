c855abe456e26c0d2fcaf1ffd730d4b634c6e961
Ben Baessler <mail@benbaessler.com>
2024-05-07 03:28:19 +0100

    feat: add api endpoint

--- lib/api.ts
+++ lib/api.ts
@@ -1,8 +1,11 @@
+import { Cast } from "@neynar/nodejs-sdk/build/neynar-api/v2";
 import { config } from "dotenv";
 config();
 
+const baseUrl = "https://api.neynar.com/v2/farcaster";
+
 export const getCasts = async (castsHashes: string[]) => {
-  const endpoint = `https://api.neynar.com/v2/farcaster/casts?casts=${castsHashes.join(
+  const endpoint = `${baseUrl}/casts?casts=${castsHashes.join(
     "%2C"
   )}&sort_type=likes`;
 
@@ -15,4 +18,23 @@ export const getCasts = async (castsHashes: string[]) => {
 
   const response = await res.json();
   return response.result.casts;
+};
+
+export const getTicketInfo = async (cast: Cast) => {
+  // TODO: Change later
+  const holderFids = cast.reactions.likes.map((user: any) => user.fid);
+  const endpoint = `${baseUrl}/user/bulk?fids=${holderFids.join("%2C")}`;
+  const res = await fetch(endpoint, {
+    headers: {
+      accept: "application/json",
+      api_key: process.env.NEXT_PUBLIC_NEYNAR_API_KEY!,
+    },
+  });
+
+  const response = await res.json();
+
+  return {
+    topHolders: response.users,
+    price: 999,
+  };
 };
