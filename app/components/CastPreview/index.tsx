93b5a2935bf079567cf48e38ce7a4d70882e5c2a
Ben Baessler <mail@benbaessler.com>
2024-05-07 01:48:02 +0100

    refactor: cast preview

--- app/components/CastPreview/index.tsx
+++ app/components/CastPreview/index.tsx
@@ -7,24 +7,27 @@ interface Props {
 }
 
 const CastPreview = ({ cast }: Props) => {
+  console.log({cast})
+
   return (
-    <div
-      className="flex gap-4 sm:w-[500px] w-[350px] flex-row items-start"
-      key={cast.hash}
-    >
-      <Avatar>
-        <AvatarImage src={cast.author.pfp_url} />
-        <AvatarFallback>CN</AvatarFallback>
-      </Avatar>
-      <div className="flex flex-col items-start w-full">
-        <div className="flex gap-2">
-          <p className="font-bold">{cast.author.display_name}</p>
-          <p className="text-gray-600">@{cast.author.username}</p>
+    <div className="flex flex-col gap-2 flex-row items-start" key={cast.hash}>
+      <div className="flex gap-3">
+        <Avatar>
+          <AvatarImage src={cast.author.pfp_url} />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        <div>
+          <p className="font-semibold">{cast.author.display_name}</p>
+          <p className="text-slate-400">@{cast.author.username} · </p>
         </div>
-        <p className="pb-2">{cast.text.replace(/https?:\/\/\S+/i, "")}</p>
-        {cast.embeds && cast.embeds.length > 0 ? (
+      </div>
+      <div>
+        <p className="pb-2 font-normal">
+          {cast.text.replace(/https?:\/\/\S+/i, "")}
+        </p>
+        {/* {cast.embeds && cast.embeds.length > 0 ? (
           <Embed _embedObject={cast.embeds[0]} />
-        ) : null}
+        ) : null} */}
       </div>
     </div>
   );
