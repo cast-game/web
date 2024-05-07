e1af97cc926fb0179c9c8844fe6da9e811dfa4e7
Ben Baessler <mail@benbaessler.com>
2024-05-07 02:43:54 +0100

    feat: time since helper

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -14,3 +14,30 @@ export function formatTimeRemaining(unixTimestamp: number): string {
     seconds
   ).padStart(2, "0")}`;
 }
+
+export function getTimeSince(date: string) {
+  const _date = new Date(date);
+  var seconds = Math.floor((Number(new Date()) - Number(_date)) / 1000);
+  var interval = Math.floor(seconds / 31536000);
+
+  if (interval > 1) {
+    return interval + "y";
+  }
+  interval = Math.floor(seconds / 2592000);
+  if (interval > 1) {
+    return interval + "mo";
+  }
+  interval = Math.floor(seconds / 86400);
+  if (interval > 1) {
+    return interval + "d";
+  }
+  interval = Math.floor(seconds / 3600);
+  if (interval > 1) {
+    return interval + "h";
+  }
+  interval = Math.floor(seconds / 60);
+  if (interval > 1) {
+    return interval + "m";
+  }
+  return Math.floor(seconds) + "s";
+}
