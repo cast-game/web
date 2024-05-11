bd9aaf48d81320fff496fcc806a825509556f009
Ben Baessler <mail@benbaessler.com>
2024-05-11 02:43:38 +0100

    chore: display landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import Navbar from "./components/Navbar";
 import Leaderboard from "./components/Leaderboard";
+import Landing from "./components/Landing";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
+      <Landing />
+      {/* <Navbar />
       <div className="flex justify-center pt-20">
         <Leaderboard />
-      </div>
+      </div> */}
     </>
   );
 }
