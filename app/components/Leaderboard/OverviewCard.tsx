d6dd546c71fa2a00990dc758d2cb5c86b8a2b68d
Ben Bassler <mail@benbaessler.com>
2024-05-27 17:21:05 +0100

    chore: redesign overview card

--- app/components/Leaderboard/OverviewCard.tsx
+++ app/components/Leaderboard/OverviewCard.tsx
@@ -1,59 +1,60 @@
 "use client";
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
 } from "@/components/ui/card";
 import { formatTimeRemaining } from "@/lib/helpers";
 import { useEffect, useState } from "react";
 
 const OverviewCard = () => {
-  const prizePoolBalance = 250871;
-  const tradeCount = 1239;
-  const deadline = 1714944818;
+	const prizePoolBalance = 250871;
+	const tradeCount = 1239;
+	const deadline = 1714944818;
 
-  const [timeRemaining, setTimeRemaining] = useState(
-    formatTimeRemaining(deadline)
-  );
+	const [timeRemaining, setTimeRemaining] = useState(
+		formatTimeRemaining(deadline)
+	);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setTimeRemaining(formatTimeRemaining(deadline));
-    }, 1000);
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setTimeRemaining(formatTimeRemaining(deadline));
+		}, 1000);
 
-    // Cleanup interval on component unmount
-    return () => clearInterval(interval);
-  }, [deadline]);
+		// Cleanup interval on component unmount
+		return () => clearInterval(interval);
+	}, [deadline]);
 
-  return (
-    <Card>
-      <CardHeader>
-        <CardDescription>Current winner prize pool:</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <div className="flex justify-between items-center">
-          <span className="text-5xl font-bold text-indigo-300">
-            {prizePoolBalance} $DEGEN
-          </span>
-          <div className="flex flex-col items-end text-lg">
-            <span className="text-slate-400">
-              Tickets bought:{" "}
-              <span className="text-white font-semibold">{tradeCount}</span>
-            </span>
-            <span className="text-slate-400">
-              Round ends in{" "}
-              <span className="text-white font-semibold">
-                {formatTimeRemaining(deadline)}
-              </span>
-            </span>
-          </div>
-        </div>
-      </CardContent>
-    </Card>
-  );
+	return (
+		<Card className="relative bg-slate-800">
+			<CardHeader>
+				<CardTitle>cast.game x /memes</CardTitle>
+				<CardDescription>Current Prize Pool:</CardDescription>
+			</CardHeader>
+			<CardContent>
+				<span className="text-5xl font-bold">{prizePoolBalance} DEGEN</span>
+				<div className="flex justify-between items-center">
+					<div className="flex flex-col items-end text-lg">
+						<div className="flex flex-col items-end text-slate-400 absolute top-5 right-7">
+							<span>
+								Round ends in{" "}
+								<span className="text-white font-semibold">
+									{formatTimeRemaining(deadline)}
+								</span>
+							</span>
+							<span className="text-slate-400">
+								Tickets bought:{" "}
+								<span className="text-white font-semibold">{tradeCount}</span>
+							</span>
+						</div>
+					</div>
+				</div>
+			</CardContent>
+		</Card>
+	);
 };
 
 export default OverviewCard;
