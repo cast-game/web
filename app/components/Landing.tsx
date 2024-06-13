d2ab7e1c5dbe1e24dd58ec2536d21d11015f833c
Ben Bassler <mail@benbaessler.com>
2024-06-13 03:07:32 +0100

    feat: update landing page

--- app/components/Landing.tsx
+++ app/components/Landing.tsx
@@ -1,13 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
+import Logo from "@/public/logo.png";
 
 const Landing = () => {
   return (
     <main className="flex items-center justify-center h-screen relative">
       <div className="flex flex-col gap-5 items-center text-center">
-        <span className="text-5xl sm:text-6xl text-[#864AF9] font-bold">
-          cast.game
-        </span>
+        <Image src={Logo} alt="cast.game" height={40} />
         <div className="flex flex-col gap-5 text-lg sm:text-xl">
           <span className=" opacity-80 max-w-sm">
             A prediction game to win $ for betting on popular casts on
@@ -17,7 +16,7 @@ const Landing = () => {
         </div>
       </div>
       <div className="flex gap-5 absolute bottom-5 transition-all">
-        <Link href="https://warpcast.com/castgame" target="_blank">
+        <Link href="https://warpcast.com/~/channel/castgame" target="_blank">
           <Image
             src="/warpcast-logo.png"
             alt="Join our channel on Farcaster"
@@ -26,7 +25,7 @@ const Landing = () => {
             className="opacity-50 hover:opacity-100 transition ease-in-out duration-200"
           />
         </Link>
-        <Link href="https://cast.game" target="_blank">
+        <Link href="https://castgame.notion.site/cast-game-spec-6ee80b83da2f4f78b46acf1a68001adf" target="_blank">
           <Image
             src="/notion-logo.png"
             alt="Read more about cast.game"
