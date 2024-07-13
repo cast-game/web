7cf6b42a8952ab4f704cfd2119707ef3a0895608
benbaessler <mail@benbaessler.com>
2024-07-13 15:34:41 +0200

    feat: game indicator

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,16 +1,25 @@
-"use client";
 import Link from "next/link";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { getChannel } from "@/lib/api";
 
-const Navbar = () => {
+
+const Navbar = async () => {
+  const channelId = "memes";
+  const channel = await getChannel(channelId);
+
   return (
     <nav className="p-3 flex justify-between items-center absolute top-0 w-full">
-      <Link href="https://warpcast.com/~/channel/tiser" target="blank">
-        <Image src="/logo.png" alt="Tiser Logo" width={45} height={45} className="rounded-full"/>
-      </Link>
+      <div className="flex items-center ml-4 gap-2">
+        <div className="rounded-full h-2 w-2 bg-green-500" />
+        <Avatar className="h-5 w-5">
+          <AvatarImage src={channel.image_url} />
+        </Avatar>
+        <span className="text-lg font-bold">{channelId}</span>
+      </div>
       <div className="flex gap-5 items-center">
-        <Link href="/">Leaderboard</Link>
+        <Link href="/">Game</Link>
         <Link href="/activity">Activity</Link>
         <Link href="/tickets">My Tickets</Link>
         <Button>Sign In With Warpcast</Button>
