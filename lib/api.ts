16b821c3a2e2cf6d267499dfa8f95680f181f752
benbaessler <mail@benbaessler.com>
2024-07-13 15:35:27 +0200

    refactor: leaderboard

--- lib/api.ts
+++ lib/api.ts
@@ -1,41 +1,57 @@
-// import { Cast } from "@neynar/nodejs-sdk/build/neynar-api/v2";
-// import { config } from "dotenv";
-// config();
+import { Cast } from "@neynar/nodejs-sdk/build/neynar-api/v2";
+import { config } from "dotenv";
+config();
 
-// const baseUrl = "https://api.neynar.com/v2/farcaster";
+const baseUrl = "https://api.neynar.com/v2/farcaster";
 
-// export const getCasts = async (castsHashes: string[]) => {
-//   console.log(castsHashes);
-//   const endpoint = `${baseUrl}/casts?casts=${castsHashes.join(
-//     "%2C"
-//   )}&sort_type=likes`;
+export const getCasts = async (castsHashes: string[]) => {
+  console.log(castsHashes);
+  const endpoint = `${baseUrl}/casts?casts=${castsHashes.join(
+    "%2C"
+  )}&sort_type=likes`;
 
-//   const res = await fetch(endpoint, {
-//     headers: {
-//       accept: "application/json",
-//       api_key: process.env.NEXT_PUBLIC_NEYNAR_API_KEY!,
-//     },
-//   });
+  const res = await fetch(endpoint, {
+    headers: {
+      accept: "application/json",
+      api_key: process.env.NEXT_PUBLIC_NEYNAR_API_KEY!,
+    },
+  });
 
-//   const response = await res.json();
-//   return response.result.casts;
-// };
+  const response = await res.json();
+  return response.result.casts;
+};
 
-// export const getTicketInfo = async (cast: Cast) => {
-//   // TODO: Change later
-//   const holderFids = cast.reactions.likes.map((user: any) => user.fid);
-//   const endpoint = `${baseUrl}/user/bulk?fids=${holderFids.join("%2C")}`;
-//   const res = await fetch(endpoint, {
-//     headers: {
-//       accept: "application/json",
-//       api_key: process.env.NEXT_PUBLIC_NEYNAR_API_KEY!,
-//     },
-//   });
+export const getChannel = async (channelId: string) => {
+  console.log({channelId})
+  const endpoint = `${baseUrl}/channel?id=${channelId}`;
 
-//   const response = await res.json();
+  const res = await fetch(endpoint, {
+    headers: {
+      accept: "application/json",
+      api_key: process.env.NEXT_PUBLIC_NEYNAR_API_KEY!,
+    },
+  });
 
-//   return {
-//     topHolders: response.users,
-//     price: 999,
-//   };
-// };
+  
+  const response = await res.json();
+  return response.channel;
+}
+
+export const getTicketInfo = async (cast: Cast) => {
+  // TODO: Change later
+  const holderFids = cast.reactions.likes.map((user: any) => user.fid);
+  const endpoint = `${baseUrl}/user/bulk?fids=${holderFids.join("%2C")}`;
+  const res = await fetch(endpoint, {
+    headers: {
+      accept: "application/json",
+      api_key: process.env.NEXT_PUBLIC_NEYNAR_API_KEY!,
+    },
+  });
+
+  const response = await res.json();
+
+  return {
+    topHolders: response.users,
+    price: 999,
+  };
+};
