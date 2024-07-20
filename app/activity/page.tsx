afb094a69e3627ddf9e84d23d164fa13997af98f
benbaessler <mail@benbaessler.com>
2024-07-20 15:38:26 +0200

    feat: activity page

---
+++ app/activity/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+
+export default function Activity() {
+  return (
+    <>
+      <Navbar selection="Activity" />
+      <div className="flex justify-center pt-20">
+        <h1>Hello World</h1>
+        {/* <Leaderboard /> */}
+      </div>
+    </>
+  );
+}
