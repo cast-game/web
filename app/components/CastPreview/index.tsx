1025ffe07cd5b84dc6cade0e02bf3b1a47eefcda
Ben Baessler <mail@benbaessler.com>
2024-08-04 17:08:53 +0100

    chore: redesign cast

--- app/components/CastPreview/index.tsx
+++ app/components/CastPreview/index.tsx
@@ -1,15 +1,10 @@
 // "use client"
-import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
-  SmallAvatar,
-} from "@/components/ui/avatar";
-import { Embed } from "./Embed";
+import Image from "next/image";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import type { Cast, User } from "@neynar/nodejs-sdk/build/neynar-api/v2";
 import { getTimeSince } from "@/lib/helpers";
-import { BadgeInfo } from "lucide-react";
-import { getTicketInfo } from "@/lib/api";
+// import { BadgeInfo } from "lucide-react";
+// import { getTicketInfo } from "@/lib/api";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import {
@@ -25,16 +20,16 @@ interface Props {
 
 const CastPreview = ({ cast }: Props) => {
   const timeSince = getTimeSince(cast.timestamp);
-  const [ticketInfo, setTicketInfo] = useState<any>(null);
+  // const [ticketInfo, setTicketInfo] = useState<any>(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await getTicketInfo(cast);
-      setTicketInfo(res);
-      console.log(ticketInfo);
-    };
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const res = await getTicketInfo(cast);
+  //     setTicketInfo(res);
+  //     console.log(ticketInfo);
+  //   };
+  //   fetchData();
+  // }, []);
 
   return (
     <Link
@@ -42,71 +37,53 @@ const CastPreview = ({ cast }: Props) => {
       target="_blank"
     >
       <div className="flex gap-4 items-center justify-between">
-        <div
-          className="flex flex-col gap-2 flex-row items-start"
-          key={cast.hash}
-        >
-          <div className="flex gap-3">
-            <Avatar>
+        <div className="flex flex-row w-full justify-between" key={cast.hash}>
+          <div className="flex">
+            <Avatar className="mr-3">
               <AvatarImage src={cast.author.pfp_url} height={20} width={20} />
               <AvatarFallback>CN</AvatarFallback>
             </Avatar>
-            <div>
-              <p className="font-semibold">{cast.author.display_name}</p>
-              <p className="text-slate-400">
-                @{cast.author.username} · {timeSince}
+            <div className="flex flex-col text-black">
+              <div className="flex">
+                <p className="font-semibold mr-1.5">
+                  {cast.author.display_name}
+                </p>
+                <p className="font-medium">
+                  @{cast.author.username} · {timeSince}
+                </p>
+              </div>
+              <p className="font-normal pr-3 line-clamp-3 mb-2">
+                {cast.text.replace(/https?:\/\/\S+/i, "")}
               </p>
+              <div className="flex gap-2">
+                <div className="flex items-center font-medium rounded-full px-3 py-1 text-sm bg-black/20">
+                  + image
+                </div>
+              </div>
             </div>
           </div>
-          <div>
-            <p className="pb-2 font-normal">
-              {cast.text.replace(/https?:\/\/\S+/i, "")}
-            </p>
-            {/* {cast.embeds && cast.embeds.length > 0 ? (
-          <Embed _embedObject={cast.embeds[0]} />
-        ) : null} */}
-          </div>
-          <TooltipProvider delayDuration={0}>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <div className="flex items-center gap-2 opacity-85 hover:opacity-100">
-                  <BadgeInfo size={22}/>
-                  <span className="flex gap-6">SCV: 235.21</span>
-                </div>
-              </TooltipTrigger>
-              <TooltipContent>
-                <p>Social Capital Value represents Lorem Ipsum.</p>
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-        </div>
-        <div className="flex flex-col items-end gap-2 text-sm text-slate-400">
-          <span>Holders (32):</span>
-          <div className="flex gap-1">
-            {ticketInfo?.topHolders.map((user: User) => (
-              <TooltipProvider key={user.username} delayDuration={0}>
-                <Tooltip>
-                  <TooltipTrigger asChild>
-                    <Link
-                      href={`https://warpcast.com/${user.username}`}
-                      target="_blank"
-                    >
-                      <SmallAvatar>
-                        <AvatarImage src={user.pfp_url} />
-                        <AvatarFallback>FC</AvatarFallback>
-                      </SmallAvatar>
-                    </Link>
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <p>@{user.username}</p>
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
-            ))}
-          </div>
-          <span>Ticket price:</span>
-          <div className="flex items-center justify-center w-36 py-1.5 bg-purple-950 text-white font-semibold rounded-sm">
-            {ticketInfo?.price} $DEGEN
+          <div className="flex flex-col justify-between">
+            <TooltipProvider delayDuration={0}>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <div className="text-sm flex self-end items-center justify-center mb-2 px-3 py-1 bg-gradient-to-r from-[#45A3B8] to-[#23B68A] text-white font-semibold rounded">
+                    21.35
+                  </div>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>Social Capital Value represents Lorem Ipsum.</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+            <div className="flex text-xl items-center gap-1 text-black font-bold py-1 px-3 bg-black/20 rounded justify-center">
+              <Image
+                src="/eth-logo.png"
+                width={20}
+                height={20}
+                alt="Ethereum logo"
+              />
+              <span>2.1</span>
+            </div>
           </div>
         </div>
       </div>
