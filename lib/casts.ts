0ae539e6a79eca8f9805440697e924973630b170
Ben Baessler <mail@benbaessler.com>
2024-08-09 15:42:18 +0100

    test: cast data

---
+++ lib/casts.ts
@@ -0,0 +1,189 @@
+export default [
+  {
+    object: "cast",
+    hash: "0x02d3f308a0f56aa39766d9f66d5c40c9aefb47f9",
+    thread_hash: "0x02d3f308a0f56aa39766d9f66d5c40c9aefb47f9",
+    parent_hash: null,
+    parent_url:
+      "chain://eip155:1/erc721:0x2A9EA02E4c2dcd56Ba20628Fe1bd46bAe2C62746",
+    root_parent_url:
+      "chain://eip155:1/erc721:0x2A9EA02E4c2dcd56Ba20628Fe1bd46bAe2C62746",
+    parent_author: {
+      fid: null,
+    },
+    author: {
+      object: "user",
+      fid: 191294,
+      custody_address: "0x114d3c173325cfa4506509eff2c2a5aecce8243e",
+      username: "benbassler.eth",
+      display_name: "Ben",
+      pfp_url: "https://i.imgur.com/WcbzMkP.png",
+      profile: {
+        bio: {
+          text: "Building /castgame, obsessed with UX. • benbassler.com",
+        },
+      },
+      follower_count: 1943,
+      following_count: 459,
+      verifications: [
+        "0xf5745cd4d5bc2e3b7352676544ea632a9a30fecd",
+        "0xa85b5383e0e82dba993747834f91fe03fccd40ab",
+      ],
+      verified_addresses: {
+        eth_addresses: [
+          "0xf5745cd4d5bc2e3b7352676544ea632a9a30fecd",
+          "0xa85b5383e0e82dba993747834f91fe03fccd40ab",
+        ],
+        sol_addresses: [],
+      },
+      active_status: "inactive",
+      power_badge: true,
+    },
+    text: "I can already tell there is going to be so much cool new stuff on FC after /farhack, can't wait!",
+    timestamp: "2024-05-04T14:20:03.000Z",
+    embeds: [],
+    reactions: {
+      likes_count: 18,
+      recasts_count: 1,
+      likes: [
+        {
+          fid: 2802,
+          fname: "garrett",
+        },
+        {
+          fid: 1407,
+          fname: "thisiszinger.eth",
+        },
+        {
+          fid: 488617,
+          fname: "saharesmailiii",
+        },
+        {
+          fid: 99,
+          fname: "jessepollak",
+        },
+        {
+          fid: 1668,
+          fname: "tomu.eth",
+        },
+      ],
+      recasts: [
+        {
+          fid: 488617,
+          fname: "saharesmailiii",
+        },
+      ],
+    },
+    replies: {
+      count: 2,
+    },
+    channel: {
+      object: "channel_dehydrated",
+      id: "farcon",
+      name: "FarCon",
+      image_url: "https://i.imgur.com/mtH8eq1.jpg",
+    },
+    mentioned_profiles: [],
+  },
+  {
+    object: "cast",
+    hash: "0x5d4ef473e8826c5a13c4218a537953bd25ae5c9c",
+    thread_hash: "0x5d4ef473e8826c5a13c4218a537953bd25ae5c9c",
+    parent_hash: null,
+    parent_url:
+      "chain://eip155:7777777/erc721:0x10a77f29a6bbeae936f3f27cd60546072dae4e41",
+    root_parent_url:
+      "chain://eip155:7777777/erc721:0x10a77f29a6bbeae936f3f27cd60546072dae4e41",
+    parent_author: {
+      fid: null,
+    },
+    author: {
+      object: "user",
+      fid: 191294,
+      custody_address: "0x114d3c173325cfa4506509eff2c2a5aecce8243e",
+      username: "benbassler.eth",
+      display_name: "Ben",
+      pfp_url: "https://i.imgur.com/WcbzMkP.png",
+      profile: {
+        bio: {
+          text: "Building /castgame, obsessed with UX. • benbassler.com",
+        },
+      },
+      follower_count: 1943,
+      following_count: 459,
+      verifications: [
+        "0xf5745cd4d5bc2e3b7352676544ea632a9a30fecd",
+        "0xa85b5383e0e82dba993747834f91fe03fccd40ab",
+      ],
+      verified_addresses: {
+        eth_addresses: [
+          "0xf5745cd4d5bc2e3b7352676544ea632a9a30fecd",
+          "0xa85b5383e0e82dba993747834f91fe03fccd40ab",
+        ],
+        sol_addresses: [],
+      },
+      active_status: "inactive",
+      power_badge: true,
+    },
+    text: "The /warpcast team this week",
+    timestamp: "2024-05-06T14:20:28.000Z",
+    embeds: [
+      {
+        url: "https://imagedelivery.net/BXluQx4ige9GuW0Ia56BHw/a99b21c1-8478-4fda-13d6-34c537be9800/original",
+        metadata: {
+          content_type: "image/png",
+          content_length: "214555",
+        },
+      },
+      {
+        cast_id: {
+          fid: 2,
+          hash: "0xc6802dba80450c951e4168e6789b184b70c0cac5",
+        },
+      },
+    ],
+    reactions: {
+      likes_count: 16,
+      recasts_count: 1,
+      likes: [
+        {
+          fid: 277561,
+          fname: "joyboyy",
+        },
+        {
+          fid: 519924,
+          fname: "salyangel",
+        },
+        {
+          fid: 896,
+          fname: "boogie",
+        },
+        {
+          fid: 411761,
+          fname: "rocksider",
+        },
+        {
+          fid: 2788,
+          fname: "leosn.eth",
+        },
+      ],
+      recasts: [
+        {
+          fid: 367887,
+          fname: "hubertle",
+        },
+      ],
+    },
+    replies: {
+      count: 2,
+    },
+    channel: {
+      object: "channel_dehydrated",
+      id: "warpcast",
+      name: "Warpcast",
+      image_url:
+        "https://ipfs.decentralized-content.com/ipfs/bafkreifezhnp5wzgabkdbkb6d65oix4r5axibupv45r7ifxphl4d6qqnry",
+    },
+    mentioned_profiles: [],
+  },
+];
