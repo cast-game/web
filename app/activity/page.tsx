467ea796a9e3385162037354356730eecbeee1fd
Ben Baessler <mail@benbaessler.com>
2024-08-10 17:16:34 +0100

    feat: connect activity data

--- app/activity/page.tsx
+++ app/activity/page.tsx
@@ -2,63 +2,100 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
-import { Cast } from "@neynar/nodejs-sdk/build/neynar-api/v2";
-import { getCasts } from "@/lib/api";
+import { Cast, User } from "@neynar/nodejs-sdk/build/neynar-api/v2";
+import { getCasts, getUsers, queryData } from "@/lib/api";
 import CastPreview from "../components/CastPreview";
 import Casts from "@/lib/casts";
+import { formatEther } from "viem";
+import { formatTimeRemaining, getTimeSince } from "@/lib/helpers";
 
 export default function Activity() {
-  const [casts, setCasts] = useState<Cast[]>(Casts as Cast[]);
+  const [casts, setCasts] = useState<Cast[] | null>();
+  const [transactions, setTransactions] = useState([]);
+  const [users, setUsers] = useState<any>(null);
 
-  // const fetchCasts = async () => {
-  //   const response = await getCasts([
-  //     "0x02d3f308a0f56aa39766d9f66d5c40c9aefb47f9",
-  //     "0x5d4ef473e8826c5a13c4218a537953bd25ae5c9c",
-  //   ]);
-  //   setCasts(response);
-  // };
+  const fetchData = async () => {
+    const response = await queryData(`{
+      transactions {
+        items {
+          castHash
+          price
+          sender
+          timestamp
+          type
+        }
+      }
+    }`);
+    setTransactions(
+      response.transactions.items.sort(
+        (a: any, b: any) => b.timestamp - a.timestamp
+      )
+    );
 
-  // useEffect(() => {
-  //   fetchCasts();
-  // }, []);
+    const castsRes = await getCasts(
+      response.transactions.items.map((tx: any) => tx.castHash)
+    );
+    setCasts(castsRes);
+
+    const addresses = Array.from(
+      new Set(response.transactions.items.map((tx: any) => tx.sender))
+    ) as string[];
+
+    const usersRes = await getUsers(addresses);
+    setUsers(usersRes);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
-    <>
-      <Navbar selection="Activity" />
-      <div className="flex justify-center pt-20">
-        <div className="flex flex-col gap-3 max-w-3xl">
-          {casts.map((cast: Cast, i: number) => (
-            <div className="p-3 bg-zinc-300 rounded" key={cast.hash}>
-              <div className="flex items-center font-medium text-black justify-between">
-                <div className="flex items-center text-lg">
-                  <a href={`https://warpcast.com/dwr`} target="_black">
-                    <div className="flex items-center hover:opacity-85 cursor-pointer">
-                      <Image
-                        src={cast.author.pfp_url!}
-                        width={35}
-                        height={35}
-                        alt={cast.author.username}
-                        className="rounded-full"
-                      />
-                      <span className="font-bold mx-2">@username</span>
-                    </div>
-                  </a>
-                  <span>
-                    purchased for <b>0.1 ETH</b>
-                  </span>
+    <div className="flex justify-center">
+      {casts && users && (
+        <div className="flex flex-col gap-4 max-w-3xl">
+          {transactions.map((tx: any, i: number) => {
+            const cast = casts.find((cast) => cast.hash === tx.castHash)!;
+            const sender = users[tx.sender.toLowerCase()][0];
+            const timeSince = getTimeSince(tx.timestamp);
+
+            return (
+              <div className="p-3 bg-zinc-300 rounded" key={i}>
+                <div className="flex items-center font-medium text-black justify-between">
+                  <div className="flex items-center text-lg">
+                    <a
+                      href={`https://warpcast.com/${sender.username}`}
+                      target="_black"
+                    >
+                      <div className="flex items-center cursor-pointer">
+                        <Image
+                          src={sender.pfp_url!}
+                          width={35}
+                          height={35}
+                          alt={sender.username}
+                          className="rounded-full"
+                        />
+                        <span className="font-bold mx-2">
+                          @{sender.username}
+                        </span>
+                      </div>
+                    </a>
+                    <span>
+                      purchased for <b>{formatEther(BigInt(tx.price))} ETH</b>
+                    </span>
+                  </div>
+                  <span className="text-sm">{timeSince}</span>
                 </div>
-                <span className="text-sm">just now</span>
+                <div
+                  className={`p-4 rounded bg-slate-200 w-full mt-3`}
+                  key={cast.hash}
+                >
+                  <CastPreview cast={cast} showPrice={false} />
+                </div>
               </div>
-              <div
-                className={`p-4 rounded bg-slate-200 w-full mt-3`}
-                key={cast.hash}
-              >
-                <CastPreview cast={cast} showPrice={false} />
-              </div>
-            </div>
-          ))}
+            );
+          })}
         </div>
-      </div>
-    </>
+      )}
+    </div>
   );
 }
