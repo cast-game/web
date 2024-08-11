cddc25bd8b10ecb4bdf6efcb50266439bac8a071
Ben Baessler <mail@benbaessler.com>
2024-08-11 15:49:37 +0100

    refactor: overview page

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,72 @@
-
 "use client";
+import Image from "next/image";
 import CastPreview from "./components/CastPreview";
 import { useEffect, useState } from "react";
-import OverviewCard from "./components/OverviewCard";
-import { getCasts } from "@/lib/api";
+import { getCasts, getDetails } from "@/lib/api";
 import { Cast } from "@neynar/nodejs-sdk/build/neynar-api/v2";
+import Casts from "@/lib/casts";
 
 const Home = () => {
-  const [sortBy, setSortBy] = useState("score");
-  const [casts, setCasts] = useState<Cast[]>([]);
+  const gameTitle = "Testnet Demo Competition";
+  const endsIn = "-";
 
-  const fetchCasts = async () => {
-    const response = await getCasts([
-      "0x02d3f308a0f56aa39766d9f66d5c40c9aefb47f9",
-      "0x5d4ef473e8826c5a13c4218a537953bd25ae5c9c",
-    ]);
-    setCasts(response);
+  const [sortBy, setSortBy] = useState("score");
+  const [data, setData] = useState({
+    rewardPool: "0",
+    transactionCount: 0,
+    userCount: 0,
+  });
+  const [casts, setCasts] = useState<Cast[]>(Casts as unknown as Cast[]);
+
+  const fetchData = async () => {
+    const response = await getDetails();
+    setData(response);
+
+    // const response = await getCasts([
+    //   "0x02d3f308a0f56aa39766d9f66d5c40c9aefb47f9",
+    //   "0x5d4ef473e8826c5a13c4218a537953bd25ae5c9c",
+    // ]);
+    // setCasts(response);
   };
 
   useEffect(() => {
-    fetchCasts();
+    fetchData();
   }, []);
 
   return (
     <div className="flex flex-col justify-center text-black">
-      <OverviewCard />
+      <div className="flex rounded items-center justify-center w-full flex-col p-8 bg-slate-200 rounded">
+        <h1 className="text-3xl font-bold">{gameTitle}</h1>
+
+        <div className="flex items-center gap-4 my-7">
+          <div className="flex flex-col items-center rounded px-6 py-3 bg-slate-300 rounded">
+            <div className="flex items-center gap-2">
+              <Image
+                src="/eth-logo.png"
+                width={27}
+                height={27}
+                alt="Ethereum logo"
+              />
+              <span className="text-3xl font-bold">{data.rewardPool}</span>
+            </div>
+            <span className="text-lg">reward pool</span>
+          </div>
+
+          <div className="flex flex-col items-center px-6 py-3 bg-slate-300 rounded">
+            <span className="text-3xl font-bold">{data.transactionCount}</span>
+            <span className="text-lg">transactions</span>
+          </div>
+
+          <div className="flex flex-col items-center px-6 py-3 bg-slate-300 rounded">
+            <span className="text-3xl font-bold">{data.userCount}</span>
+            <span className="text-lg">participants</span>
+          </div>
+        </div>
+
+        <p className="text-xl font-medium">
+          Ends in <span className="font-bold">{endsIn}</span>
+        </p>
+      </div>
       <div className="mt-12 gap-4">
         <div className="flex items-center gap-5 text-slate-300">
           <span className="text-2xl font-medium">Top Casts</span>
@@ -68,6 +110,6 @@ const Home = () => {
       </div>
     </div>
   );
-}
+};
 
 export default Home;
