21b45a61545fc530859168ce0ff2f72b4584a7ac
Ben Bassler <mail@benbaessler.com>
2024-08-15 22:26:11 +0100

    refactor: responsive cast embed

--- app/components/CastPreview/index.tsx
+++ app/components/CastPreview/index.tsx
@@ -48,24 +48,25 @@ const CastPreview = ({ castData, showPrice = true }: Props) => {
     <Link
       href={`https://warpcast.com/${cast.author.username}/${cast.hash}`}
       target="_blank"
+      rel="noopener noreferrer"
     >
-      <div className="flex flex-row w-full justify-between" key={cast.hash}>
+      <div className="flex flex-col md:flex-row w-full justify-between gap-4 md:gap-6" key={cast.hash}>
         <div className="flex">
-          <Avatar className="mr-3">
+          <Avatar className="mr-3 shrink-0">
             <AvatarImage src={cast.author.pfp_url} />
             <AvatarFallback>@{cast.author.username}</AvatarFallback>
           </Avatar>
-          <div className="flex flex-col text-black">
-            <div className="flex">
+          <div className="flex flex-col text-black sm:text-lg text-md">
+            <div className="flex flex-wrap items-center">
               <p className="font-semibold mr-1.5">{cast.author.display_name}</p>
-              <p className="font-medium">
+              <p className="font-medium text-gray-600">
                 @{cast.author.username} · {timeSince}
               </p>
             </div>
             <p className="font-medium pr-3 line-clamp-3 mb-2">
               {cast.text.replace(/https?:\/\/\S+/i, "")}
             </p>
-            <div className="flex gap-2">
+            <div className="flex flex-wrap gap-2">
               {embeds.map((embed: string) => (
                 <div
                   key={embed}
@@ -77,11 +78,11 @@ const CastPreview = ({ castData, showPrice = true }: Props) => {
             </div>
           </div>
         </div>
-        <div className="flex flex-col justify-between ml-3">
+        <div className="flex md:flex-col items-center sm:justify-between justify-end ml-3 gap-3 md:gap-0">
           <TooltipProvider delayDuration={0}>
             <Tooltip>
               <TooltipTrigger asChild>
-                <div className="text-sm flex self-end items-center justify-center mb-2 px-3 py-1 bg-gradient-to-r from-[#45A3B8] to-[#23B68A] text-white font-semibold rounded-full">
+                <div className="text-sm flex self-end items-center justify-center sm:mb-2 px-3 py-1 bg-gradient-to-r from-[#45A3B8] to-[#23B68A] text-white font-semibold rounded-full">
                   {socialCapitalValue}
                 </div>
               </TooltipTrigger>
@@ -91,7 +92,7 @@ const CastPreview = ({ castData, showPrice = true }: Props) => {
             </Tooltip>
           </TooltipProvider>
           {showPrice && (
-            <div className="flex text-xl items-center gap-1 text-black font-bold py-1 px-3 bg-black/20 rounded justify-center">
+            <div className="flex items-center gap-1 text-xl text-black font-bold py-1 px-3 bg-black/20 rounded justify-center">
               <Image
                 src="/eth-logo.png"
                 width={20}
@@ -105,6 +106,7 @@ const CastPreview = ({ castData, showPrice = true }: Props) => {
       </div>
     </Link>
   );
+  
 };
 
 export default CastPreview;
