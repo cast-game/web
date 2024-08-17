94dc25be9e8df0ff608370756c4d78f649fa4372
Ben Bassler <mail@benbaessler.com>
2024-08-17 15:25:58 +0100

    feat: lazy load activity + tickets

--- app/activity/page.tsx
+++ app/activity/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
 import {
 	getCasts,
 	getSCVQuery,
@@ -21,10 +22,18 @@ const Activity = () => {
 	const [casts, setCasts] = useState<CastData[] | null>(null);
 	const [transactions, setTransactions] = useState<any[] | null>(null);
 	const [users, setUsers] = useState<User[] | null>(null);
+	const [endCursor, setEndCursor] = useState<string | null>(null);
+	const [hasNextPage, setHasNextPage] = useState(true);
+	const { ref, inView } = useInView();
 
-	const fetchData = async () => {
+	const fetchData = async (cursor: string | null) => {
 		const response = await queryData(`{
-      transactions {
+      transactions(
+        limit: 10,
+        ${cursor ? `after: "${cursor}",` : ""}
+        orderBy: "timestamp",
+        orderDirection: "desc"
+      ) {
         items {
           castHash
           price
@@ -32,21 +41,27 @@ const Activity = () => {
           timestamp
           type
         }
+        pageInfo {
+          endCursor
+          hasNextPage
+        }
       }
     }`);
 
-		setTransactions(
-			response.transactions.items.sort(
-				(a: any, b: any) => b.timestamp - a.timestamp
-			)
+		const newTransactions = response.transactions.items;
+		setEndCursor(response.transactions.pageInfo.endCursor);
+		setHasNextPage(response.transactions.pageInfo.hasNextPage);
+
+		setTransactions((prevTransactions) =>
+			prevTransactions
+				? [...prevTransactions, ...newTransactions]
+				: newTransactions
 		);
 
-		const castsHashes = response.transactions.items.map(
-			(tx: any) => tx.castHash
-		);
+		const castsHashes = newTransactions.map((tx: any) => tx.castHash);
 
 		const fids = Array.from(
-			new Set(response.transactions.items.map((tx: any) => tx.senderFid))
+			new Set(newTransactions.map((tx: any) => tx.senderFid))
 		) as number[];
 
 		const [usersRes, castsRes, scvQueryData] = await Promise.all([
@@ -57,75 +72,89 @@ const Activity = () => {
 
 		const castScores = handleSCVData(scvQueryData.data.FarcasterCasts.Cast);
 
-		setUsers(usersRes.users);
-		setCasts(
-			castsRes.map((cast: any, i: number) => {
-				return {
-					cast,
-					price: 0,
-					socialCapitalValue: castScores.find((c: any) => c.hash === cast.hash)
-						.score,
-				};
-			})
+		setUsers((prevUsers) =>
+			prevUsers ? [...prevUsers, ...usersRes.users] : usersRes.users
 		);
+		setCasts((prevCasts) => {
+			const newCasts = castsRes.map((cast: any, i: number) => ({
+				cast,
+				price: 0,
+				socialCapitalValue: castScores.find((c: any) => c.hash === cast.hash)
+					.score,
+			}));
+			return prevCasts ? [...prevCasts, ...newCasts] : newCasts;
+		});
 	};
 
 	useEffect(() => {
-		fetchData();
+		fetchData(null);
 	}, []);
+
+	useEffect(() => {
+		if (inView && hasNextPage) {
+			fetchData(endCursor);
+		}
+	}, [inView, hasNextPage, endCursor]);
 
 	return (
 		<div className="flex justify-center">
 			<div className="flex flex-col gap-4 w-full">
 				{transactions && casts && users ? (
-					transactions.map((tx: any, i: number) => {
-						const cast = casts
-							.map((c: any) => c.cast)
-							.find((c: any) => c.hash === tx.castHash);
-						const sender = users.find(
-							(user: any) => user.fid === Number(tx.senderFid)
-						);
-						const timeSince = getTimeSince(tx.timestamp);
+					<>
+						{transactions.map((tx: any, i: number) => {
+							const castData = casts
+								.find((c: any) => c.cast.hash === tx.castHash);
+							const sender = users.find(
+								(user: any) => user.fid === Number(tx.senderFid)
+							);
+							const timeSince = getTimeSince(tx.timestamp);
 
-						return (
-							<div className="p-3 bg-zinc-300 rounded" key={i}>
-								<div className="flex items-center font-medium text-black justify-between">
-									<div className="flex items-center">
-										<a
-											href={`https://warpcast.com/${sender?.username}`}
-											target="_black"
-										>
-											<div className="flex items-center cursor-pointer">
-												<Image
-													src={sender?.pfp_url!}
-													width={30}
-													height={30}
-													alt={sender?.username!}
-													className="rounded-full"
-												/>
-												<span className="font-bold mr-1 ml-2">
-													@{sender?.username}
-												</span>
-											</div>
-										</a>
-										<span>
-											{tx.type === "buy" ? "purchased" : "sold"} for{" "}
-											<b>{formatEther(BigInt(tx.price))} ETH</b>
-										</span>
+							return (
+								<div className="p-3 bg-zinc-300 rounded" key={i}>
+									<div className="flex items-center font-medium text-black justify-between">
+										<div className="flex items-center">
+											<a
+												href={`https://warpcast.com/${sender?.username}`}
+												target="_blank"
+												rel="noopener noreferrer"
+											>
+												<div className="flex items-center cursor-pointer">
+													<Image
+														src={sender?.pfp_url!}
+														width={30}
+														height={30}
+														alt={sender?.username!}
+														className="rounded-full"
+													/>
+													<span className="font-bold mr-1 ml-2">
+														@{sender?.username}
+													</span>
+												</div>
+											</a>
+											<span>
+												{tx.type === "buy" ? "purchased" : "sold"} for{" "}
+												<b>{formatEther(BigInt(tx.price))} ETH</b>
+											</span>
+										</div>
+										<span className="text-sm">{`${timeSince} ${
+											timeSince === "just now" ? "" : "ago"
+										}`}</span>
 									</div>
-									<span className="text-sm">{`${timeSince} ${
-										timeSince === "just now" ? "" : " ago"
-									}`}</span>
+									<div
+										className={`p-3 rounded bg-slate-200 w-full mt-3`}
+										key={i}
+									>
+										<CastPreview castData={castData!} showPrice={false} />
+									</div>
 								</div>
-								<div
-									className={`p-3 rounded bg-slate-200 w-full mt-3`}
-									key={cast.hash}
-								>
-									<CastPreview castData={casts[i]} showPrice={false} />
-								</div>
+							);
+						})}
+						{hasNextPage && (
+							<div ref={ref} className="flex justify-center items-center p-4">
+								<Spinner size="3" />
 							</div>
-						);
-					})
+						)}
+					</>
 				) : (
 					<div className="flex justify-center items-center pt-16">
 						<Spinner size="3" />
--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import CastPreview from "../components/CastPreview";
 import { usePrivy } from "@privy-io/react-auth";
 import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
 import {
 	getCasts,
 	getSCVQuery,
@@ -16,51 +17,87 @@ import { formatEther } from "viem";
 import { Spinner } from "@radix-ui/themes";
 init(process.env.NEXT_PUBLIC_AIRSTACK_API_KEY!);
 
+const ITEMS_PER_PAGE = 10;
+
 const Tickets = () => {
 	const { user } = usePrivy();
 	const [data, setData] = useState<CastData[] | null>(null);
+	const [endCursor, setEndCursor] = useState<string | null>(null);
+	const [hasNextPage, setHasNextPage] = useState(true);
+	const { ref, inView } = useInView();
+	const [addresses, setAddresses] = useState<string[]>([]);
 
-	const fetchData = async () => {
+	const fetchUserData = async () => {
 		const res = await getUsers([user!.farcaster?.fid!]);
 		const userData = res.users[0];
-		const addresses = [
+		const userAddresses = [
 			userData.custody_address,
 			...userData.verified_addresses.eth_addresses,
 		];
+		setAddresses(userAddresses);
+		return userAddresses;
+	};
 
-		const results = await Promise.all(
-			addresses.map(async (address: string) => {
-				const output = await queryData(`{
-          users(
-            where: {
-              id_starts_with: "${address}"
-              ticketBalance_gt: "0"
-            }
-          ) {
-            items {
-              ticketBalance
-              id
-            }
+	const fetchTickets = async (
+		userAddresses: string[],
+		cursor: string | null
+	) => {
+		const queryPromises = userAddresses.map(async (address: string) => {
+			const output = await queryData(`{
+        users(
+          limit: ${ITEMS_PER_PAGE},
+          ${cursor ? `after: "${cursor}",` : ""}
+          where: {
+            id_starts_with: "${address}"
+            ticketBalance_gt: "0"
           }
-        }`);
-				return output.users.items;
-			})
-		);
+        ) {
+          items {
+            ticketBalance
+            id
+          }
+          pageInfo {
+            endCursor
+            hasNextPage
+          }
+        }
+      }`);
+			return output.users;
+		});
 
-		let balances: any[] = [];
-		results.forEach((items) => {
-			items.forEach((item: any) => {
+		const results = await Promise.all(queryPromises);
+
+		let newBalances: any[] = [];
+		let newEndCursor: string | null = null;
+		let newHasNextPage = false;
+
+		results.forEach((result) => {
+			result.items.forEach((item: any) => {
 				const castHash = item.id.split(":")[1];
-				const existingEntry = balances.find(
+				const existingEntry = newBalances.find(
 					(entry) => entry.castHash === castHash
 				);
 				if (existingEntry) {
-					return (existingEntry.balance += Number(item.ticketBalance));
+					existingEntry.balance += Number(item.ticketBalance);
+				} else {
+					newBalances.push({ castHash, balance: Number(item.ticketBalance) });
 				}
-				balances.push({ castHash, balance: Number(item.ticketBalance) });
 			});
+
+			if (result.pageInfo.hasNextPage) {
+				newHasNextPage = true;
+				newEndCursor = result.pageInfo.endCursor;
+			}
 		});
 
+		return {
+			balances: newBalances,
+			endCursor: newEndCursor,
+			hasNextPage: newHasNextPage,
+		};
+	};
+
+	const fetchCastData = async (balances: any[]) => {
 		const castsHashes = balances.map((b) => b.castHash);
 		const [scvRes, castsRes, ticketsRes] = await Promise.all([
 			fetchQuery(getSCVQuery(castsHashes)),
@@ -72,7 +109,7 @@ const Tickets = () => {
             id
           }
         }
-			}`),
+      }`),
 		]);
 
 		const castScores = handleSCVData(scvRes.data.FarcasterCasts.Cast);
@@ -88,48 +125,85 @@ const Tickets = () => {
 				price: formatEther(ticket.buyPrice),
 			};
 		});
-		setData(castData);
+
+		return castData;
+	};
+
+	const fetchData = async (cursor: string | null) => {
+		if (!addresses.length) {
+			const userAddresses = await fetchUserData();
+			const {
+				balances,
+				endCursor: newEndCursor,
+				hasNextPage: newHasNextPage,
+			} = await fetchTickets(userAddresses, cursor);
+			const castData = await fetchCastData(balances);
+
+			setData(castData);
+			setEndCursor(newEndCursor);
+			setHasNextPage(newHasNextPage);
+		} else {
+			const {
+				balances,
+				endCursor: newEndCursor,
+				hasNextPage: newHasNextPage,
+			} = await fetchTickets(addresses, cursor);
+			const castData = await fetchCastData(balances);
+
+			setData((prevData) => (prevData ? [...prevData, ...castData] : castData));
+			setEndCursor(newEndCursor);
+			setHasNextPage(newHasNextPage);
+		}
 	};
 
 	useEffect(() => {
-		if (user) fetchData();
+		if (user) fetchData(null);
 	}, [user]);
+
+	useEffect(() => {
+		if (inView && hasNextPage) {
+			fetchData(endCursor);
+		}
+	}, [inView, hasNextPage]);
 
 	return (
 		<div className="flex justify-center">
 			{data ? (
 				<div className="flex flex-col gap-4">
-					{data.map((castData: CastData, i: number) => {
-						return (
-							<div className="p-3 bg-zinc-300 rounded" key={i}>
-								<div className="flex items-center text-black justify-between">
-									<div className="flex items-center text-lg font-bold">
-										<div className="flex gap-3 items-center">
-											<div className="flex text-xl items-center gap-1 text-black font-bold py-1 px-3 bg-black/20 rounded justify-center">
-												<Image
-													src="/eth-logo.png"
-													width={20}
-													height={20}
-													alt="Ethereum logo"
-												/>
-												<span>{castData.price}</span>
-											</div>
-											{/* <span className="text-lime-600">+100%</span> */}
+					{data.map((castData: CastData, i: number) => (
+						<div className="p-3 bg-zinc-300 rounded" key={i}>
+							<div className="flex items-center text-black justify-between">
+								<div className="flex items-center text-lg font-bold">
+									<div className="flex gap-3 items-center">
+										<div className="flex text-xl items-center gap-1 text-black font-bold py-1 px-3 bg-black/20 rounded justify-center">
+											<Image
+												src="/eth-logo.png"
+												width={20}
+												height={20}
+												alt="Ethereum logo"
+											/>
+											<span>{castData.price}</span>
 										</div>
+										{/* <span className="text-lime-600">+100%</span> */}
 									</div>
-									{castData.balance! > 1 && (
-										<span className="text-lg font-bold">{`${castData.balance}x`}</span>
-									)}
 								</div>
-								<div
-									className={`p-4 rounded bg-slate-200 w-full mt-3`}
-									key={castData.cast.hash}
-								>
-									<CastPreview castData={castData} showPrice={false} />
-								</div>
+								{castData.balance! > 1 && (
+									<span className="text-lg font-bold">{`${castData.balance}x`}</span>
+								)}
 							</div>
-						);
-					})}
+							<div
+								className={`p-4 rounded bg-slate-200 w-full mt-3`}
+								key={castData.cast.hash}
+							>
+								<CastPreview castData={castData} showPrice={false} />
+							</div>
+						</div>
+					))}
+					{hasNextPage && (
+						<div ref={ref} className="flex justify-center items-center p-4">
+							<Spinner size="3" />
+						</div>
+					)}
 				</div>
 			) : (
 				<div className="flex justify-center items-center pt-16">
